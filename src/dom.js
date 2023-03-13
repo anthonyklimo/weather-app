59b8cea75362bee0f39a8fceb0e362ba8c4da806
Anthony <cooperkl317@gmail.com>
2023-03-12 19:54:33 -0700

    add function to render api data on to html elements

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,22 @@
-function renderPagePage() {
+function renderPage(weatherData) {
+  const location = document.getElementById('location');
+  const weather = document.getElementById('weather');
+  const temperature = document.getElementById('temperature');
+  const feelsLike = document.getElementById('feels-like');
+  const humidity = document.getElementById('humidity');
+  const high = document.getElementById('high');
+  const low = document.getElementById('low');
 
+  location.textContent = weatherData.location;
+  weather.textContent = weatherData.weather;
+  temperature.textContent = weatherData.temperature;
+  feelsLike.textContent = weatherData.feelsLike;
+  humidity.textContent = weatherData.humidity;
+  high.textContent = weatherData.high;
+  low.textContent = weatherData.low;
+
+  console.log(location.textContent);
+  console.log(weatherData.location);
 }
+
+export default renderPage;
